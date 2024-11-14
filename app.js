40230291568ddf688a0492d186bef0487444e87b
Ayush Pradhan <154524186+Aayushpradhan88-js@users.noreply.github.com>
2024-11-14 09:54:27 +0000

    handling error

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
     fs.readdir(`./files`, (err, files) => {
-        res.render('form', {files: files});
+        res.render('Home', {files: files});
     })
 
 })
@@ -29,7 +29,7 @@ app.post('/create', (req, res) => {
 
 app.get('/file/:filename', (req, res) => {
     fs.readFile(`./files/${req.params.filename}`,"utf-8" ,(err, filedata) => {
-        res.render('readFile', {filename: req.params.filename, filedata: filedata});
+        res.render('readfile', {filename: req.params.filename, filedata: filedata});
     })
 })
 
@@ -43,15 +43,6 @@ app.post('/edit', (req, res) => {
     fs.rename(`./files/${req.body.Previous}`, `./files/${req.body.New}`, (err) => {
         res.redirect("/");
     })
-})
-
-//menu feature ---
-
-const hov = document.querySelector('#hover');
-const btn = document.querySelector('#menu');
-
-btn.addEventListener('click', () => {
-    
 })
 
 const port = 3000
